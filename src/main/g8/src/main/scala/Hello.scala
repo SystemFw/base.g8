d80d554f650164388464267ea13b32b2ce791a81
fabio labella <fabio.labella@ncr.com>
2017-02-13 21:56:09 +0000

    Add improved stub classes

---
+++ src/main/g8/src/main/scala/Hello.scala
@@ -0,0 +1,5 @@
+package $organization$.$name;format="lower,word"$
+
+object Hello {
+  def hello(name: String) = "Hello " + name
+}
---
+++ src/main/g8/src/test/scala/HelloSpec.scala
@@ -0,0 +1,12 @@
+package $organization$.$name;format="lower,word"$
+
+import org.specs2.mutable.Specification
+import org.specs2.ScalaCheck
+
+class HelloSpec extends Specification with ScalaCheck {
+  "Hello" should {
+    "say hello" in prop { name: String =>
+      Hello.hello(name) must beEqualTo("Hello " + name)
+    }
+  }
+}
